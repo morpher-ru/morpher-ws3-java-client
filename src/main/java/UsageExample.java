eefe278c4834d1c911ea91cd47a33971d047b5c6
andrii-r <atomix@kievnet.com.ua>
2017-11-14 23:37:20 +0200

    Added correction methods to Ukrainian client, add ukrainian examples

--- src/main/java/UsageExample.java
+++ src/main/java/UsageExample.java
@@ -4,6 +4,7 @@ import clients.russian.data.CorrectionEntry;
 import clients.russian.data.CorrectionForms;
 import clients.russian.data.DeclensionResult;
 import clients.russian.data.NumberSpellingResult;
+import clients.ukrainian.UkrainianClient;
 import exceptions.MorpherException;
 
 import java.io.IOException;
@@ -34,10 +35,13 @@ public class UsageExample {
 
         try {
             runRussianClientExamples(morpherClient);
+            runUkrainianClientExamples(morpherClient);
 
         } catch (MorpherException e) {
             e.printStackTrace();
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
@@ -52,6 +56,14 @@ public class UsageExample {
         russianAdjectivizeExample(russianClient);
         russianSpellWithCorrectionExample(russianClient);
         russianNumberSpellingResult(russianClient);
+    }
+
+    private static void runUkrainianClientExamples(MorpherClient morpherClient) throws MorpherException, IOException {
+        UkrainianClient ukrainianClient = morpherClient.ukrainian();
+
+        ukrainianDeclensionsAndGenderExample(ukrainianClient);
+        ukrainianNumberSpellingResult(ukrainianClient);
+        ukrainianSpellWithCorrectionExample(ukrainianClient);
     }
 
     private static void russianSpellWithCorrectionExample(RussianClient russianClient) throws MorpherException, IOException {
@@ -90,12 +102,14 @@ public class UsageExample {
             log("   Т: " + correction.singular.instrumental);
             log("   П: " + correction.singular.prepositional);
             log("   М: " + correction.singular.locative);
-            log("   М_Р: " + correction.plural.genitive);
-            log("   М_Д: " + correction.plural.dative);
-            log("   М_В: " + correction.plural.accusative);
-            log("   М_Т: " + correction.plural.instrumental);
-            log("   М_П: " + correction.plural.prepositional);
-            log("   М_М: " + correction.plural.locative);
+            if (correction.plural != null) {
+                log("   М_Р: " + correction.plural.genitive);
+                log("   М_Д: " + correction.plural.dative);
+                log("   М_В: " + correction.plural.accusative);
+                log("   М_Т: " + correction.plural.instrumental);
+                log("   М_П: " + correction.plural.prepositional);
+                log("   М_М: " + correction.plural.locative);
+            }
         }
         log("");
 
@@ -181,6 +195,87 @@ public class UsageExample {
         log("Ф: " + nameResult.fullName.surname);
         log("И: " + nameResult.fullName.name);
         log("О: " + nameResult.fullName.patronymic);
+        log("");
+    }
+
+    private static void ukrainianSpellWithCorrectionExample(UkrainianClient ukrainianClient) throws MorpherException, IOException {
+        // Функции пользовательского словаря для ws3.morpher.ru работают только при наличии токена.
+        // Для local сервиса токен не нужен.
+
+        // Украинский язык
+        // Добавляем новое пользовательское исправление
+        clients.ukrainian.data.CorrectionForms singular = new clients.ukrainian.data.CorrectionForms();
+        singular.nominative = "Сергій";
+        singular.prepositional = "Сергієві";
+
+        clients.ukrainian.data.CorrectionEntry correctionEntry = new clients.ukrainian.data.CorrectionEntry();
+        correctionEntry.singular = singular;
+
+        ukrainianClient.addOrUpdateToUserDict(correctionEntry);
+
+        log("Склонение с исправлением:");
+        clients.ukrainian.data.DeclensionResult spellWithCorrection = ukrainianClient.declension("Сергій");
+        log("Називний вiдмiнок: %s", spellWithCorrection.nominative);
+        log("Мiсцевий вiдмiнок: %s", spellWithCorrection.prepositional);
+        log("");
+
+        log("Получаем список всех исправлений:");
+        List<clients.ukrainian.data.CorrectionEntry> corrections = ukrainianClient.fetchAllFromUserDictionary();
+        for (clients.ukrainian.data.CorrectionEntry correction : corrections) {
+            log(correction.singular.nominative + ":");
+            log("   Р: " + correction.singular.genitive);
+            log("   Д: " + correction.singular.dative);
+            log("   З: " + correction.singular.accusative);
+            log("   О: " + correction.singular.instrumental);
+            log("   М: " + correction.singular.prepositional);
+            log("   К: " + correction.singular.vocative);
+            if (correction.plural != null) {
+                log("   М_Р: " + correction.plural.genitive);
+                log("   М_Д: " + correction.plural.dative);
+                log("   М_З: " + correction.plural.accusative);
+                log("   М_О: " + correction.plural.instrumental);
+                log("   М_М: " + correction.plural.prepositional);
+                log("   М_К: " + correction.plural.vocative);
+            }
+        }
+        log("");
+
+        // Удаляем исправление.
+        // True если исправление было удалено успешно, false если исправление не найдено.
+        boolean found = ukrainianClient.removeFromUserDictionary("Сергій");
+        log("Исправление найдено: %s", found ? "Да" : "Нет");
+
+        log("Склонение после удаления исправления:");
+        clients.ukrainian.data.DeclensionResult spellWithoutCorrection = ukrainianClient.declension("Сергій");
+        log("Називний вiдмiнок: %s", spellWithoutCorrection.nominative);
+        log("Мiсцевий вiдмiнок: %s", spellWithoutCorrection.prepositional);
+        log("");
+    }
+
+    private static void ukrainianNumberSpellingResult(UkrainianClient ukrainianClient) throws MorpherException, IOException {
+        log("Сумма прописью на укранинском:");
+        int number = 2513;
+        clients.ukrainian.data.NumberSpellingResult ukrainianNumberSpellingResult = ukrainianClient.spell(number, "рубль");
+        log("У розмірі %s (%s) %s", number,
+                ukrainianNumberSpellingResult.numberDeclension.genitive,
+                ukrainianNumberSpellingResult.unitDeclension.genitive);
+        log("");
+    }
+
+    private static void ukrainianDeclensionsAndGenderExample(UkrainianClient ukrainianClient) throws MorpherException, IOException {
+        log("Склонение ФИО на украинском языке:");
+        clients.ukrainian.data.DeclensionResult ukrainianDeclensionResult = ukrainianClient.declension("Тест");
+        log(" Називний вiдмiнок: " + ukrainianDeclensionResult.nominative);
+        log("  Родовий вiдмiнок: " + ukrainianDeclensionResult.genitive);
+        log("Давальний вiдмiнок: " + ukrainianDeclensionResult.dative);
+        log("Знахідний вiдмiнок: " + ukrainianDeclensionResult.accusative);
+        log("  Орудний вiдмiнок: " + ukrainianDeclensionResult.instrumental);
+        log(" Місцевий вiдмiнок: " + ukrainianDeclensionResult.prepositional);
+        log("  Кличний вiдмiнок: " + ukrainianDeclensionResult.vocative);
+        log("");
+
+        log("Определение рода на украинском языке:");
+        log("Род: %s", ukrainianDeclensionResult.gender != null ? ukrainianDeclensionResult.gender : PREMIUM);
         log("");
     }
 
--- src/main/java/clients/ukrainian/UkrainianClient.java
+++ src/main/java/clients/ukrainian/UkrainianClient.java
@@ -1,16 +1,21 @@
 package clients.ukrainian;
 
 import clients.AbstractLanguageClient;
+import clients.ukrainian.data.CorrectionEntry;
 import clients.ukrainian.data.DeclensionResult;
 import clients.ukrainian.data.NumberSpellingResult;
+import com.fasterxml.jackson.core.type.TypeReference;
 import communicator.Communicator;
 import exceptions.MorpherException;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import static communicator.Communicator.HTTP_METHOD_DELETE;
 import static communicator.Communicator.HTTP_METHOD_GET;
+import static communicator.Communicator.HTTP_METHOD_POST;
 
 public class UkrainianClient extends AbstractLanguageClient {
 
@@ -36,6 +41,43 @@ public class UkrainianClient extends AbstractLanguageClient {
         params.put("unit", unit);
 
         return sendRequest("spell", params, HTTP_METHOD_GET, NumberSpellingResult.class);
+    }
+
+    public void addOrUpdateToUserDict(CorrectionEntry correctionEntry) throws MorpherException, IOException {
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("Н", correctionEntry.singular.nominative);
+        params.put("Р", correctionEntry.singular.genitive);
+        params.put("Д", correctionEntry.singular.dative);
+        params.put("З", correctionEntry.singular.accusative);
+        params.put("О", correctionEntry.singular.instrumental);
+        params.put("М", correctionEntry.singular.prepositional);
+        params.put("К", correctionEntry.singular.vocative);
+
+
+        if (correctionEntry.plural != null) {
+            //Plural population
+            params.put("М_Н", correctionEntry.plural.nominative);
+            params.put("М_Р", correctionEntry.plural.genitive);
+            params.put("М_Д", correctionEntry.plural.dative);
+            params.put("М_З", correctionEntry.plural.accusative);
+            params.put("М_О", correctionEntry.plural.instrumental);
+            params.put("М_М", correctionEntry.plural.prepositional);
+            params.put("М_К", correctionEntry.plural.vocative);
+        }
+
+        sendRequest("userdict", params, HTTP_METHOD_POST);
+    }
+
+    public List<CorrectionEntry> fetchAllFromUserDictionary() throws MorpherException, IOException {
+        return sendRequest("userdict", null, HTTP_METHOD_GET, new TypeReference<List<CorrectionEntry>>() {
+        });
+    }
+
+    public boolean removeFromUserDictionary(String nominativeCorrection) throws MorpherException, IOException {
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("s", nominativeCorrection);
+
+        return sendRequest("userdict", params, HTTP_METHOD_DELETE, Boolean.class);
     }
 
     @Override
