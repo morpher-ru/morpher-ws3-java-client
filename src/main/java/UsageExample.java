5107995caa7b190c0e2f9893c2c863181462b322
Sergey Slepov <slepov@gmail.com>
2017-12-07 08:30:21 +0000

    Exemplary exception handling.

--- src/main/java/UsageExample.java
+++ src/main/java/UsageExample.java
@@ -43,13 +43,12 @@ public class UsageExample {
             demo(morpherClient.ukrainian());
         } catch (IOException e) {
             e.printStackTrace();
-        } catch (Exception e) {
+        } catch (AccessDeniedException e) {
             e.printStackTrace();
         }
-
     }
 
-    private static void demo(RussianClient russianClient) throws IOException, NumeralsDeclensionNotSupportedException, ArgumentNotRussianException, InvalidFlagsException, ArgumentEmptyException, AccessDeniedException {
+    private static void demo(RussianClient russianClient) throws IOException, AccessDeniedException {
         russianDeclensionsAndGenderExample(russianClient);
         russianNamesDeclensionsExample(russianClient);
         russianAdjectivesGendersExample(russianClient);
@@ -64,65 +63,78 @@ public class UsageExample {
         ukrainianSpellWithCorrectionExample(ukrainianClient);
     }
 
-    private static void russianSpellWithCorrectionExample(RussianClient russianClient) throws IOException, NumeralsDeclensionNotSupportedException, ArgumentNotRussianException, InvalidFlagsException, ArgumentEmptyException, AccessDeniedException {
-        // Функции пользовательского словаря для ws3.morpher.ru работают только при наличии токена.
-        // Для local сервиса токен не нужен.
+    private static void russianSpellWithCorrectionExample(RussianClient russianClient) throws IOException, AccessDeniedException {
+        try {
+            // Функции пользовательского словаря для ws3.morpher.ru работают только при наличии токена.
+            // Для local сервиса токен не нужен.
 
-        // Русский язык
-        // Добавляем новое пользовательское исправление
-        CorrectionForms singular = new CorrectionForms();
-        singular.nominative = "Кошка";
-        singular.dative = "Пантере";
+            // Русский язык
+            // Добавляем новое пользовательское исправление
+            CorrectionForms singular = new CorrectionForms();
+            singular.nominative = "Кошка";
+            singular.dative = "Пантере";
 
-        CorrectionForms plural = new CorrectionForms();
-        plural.dative = "Пантерам";
+            CorrectionForms plural = new CorrectionForms();
+            plural.dative = "Пантерам";
 
-        CorrectionEntry correctionEntry = new CorrectionEntry();
-        correctionEntry.singular = singular;
-        correctionEntry.plural = plural;
+            CorrectionEntry correctionEntry = new CorrectionEntry();
+            correctionEntry.singular = singular;
+            correctionEntry.plural = plural;
 
-        russianClient.addOrUpdateUserDict(correctionEntry);
+            russianClient.addOrUpdateUserDict(correctionEntry);
 
-        log("Склонение с исправлением:");
-        DeclensionResult declensionWithCorrection = russianClient.declension("Кошка");
-        log("            Именительный падеж: %s", declensionWithCorrection.nominative);
-        log("               Дательный падеж: %s", declensionWithCorrection.dative);
-        log("Дательный падеж множественного: %s", declensionWithCorrection.plural.dative);
-        log("");
+            log("Склонение с исправлением:");
+            DeclensionResult declensionWithCorrection = russianClient.declension("Кошка");
+            log("            Именительный падеж: %s", declensionWithCorrection.nominative);
+            log("               Дательный падеж: %s", declensionWithCorrection.dative);
+            log("Дательный падеж множественного: %s", declensionWithCorrection.plural.dative);
+            log("");
 
-        log("Получаем список всех исправлений:");
-        List<CorrectionEntry> corrections = russianClient.fetchAllFromUserDictionary();
-        for (CorrectionEntry correction : corrections) {
-            log(correction.singular.nominative + ":");
-            log("   Р: " + correction.singular.genitive);
-            log("   Д: " + correction.singular.dative);
-            log("   В: " + correction.singular.accusative);
-            log("   Т: " + correction.singular.instrumental);
-            log("   П: " + correction.singular.prepositional);
-            log("   М: " + correction.singular.locative);
-            if (correction.plural != null) {
-                log("   М_Р: " + correction.plural.genitive);
-                log("   М_Д: " + correction.plural.dative);
-                log("   М_В: " + correction.plural.accusative);
-                log("   М_Т: " + correction.plural.instrumental);
-                log("   М_П: " + correction.plural.prepositional);
-                log("   М_М: " + correction.plural.locative);
+            log("Получаем список всех исправлений:");
+            List<CorrectionEntry> corrections = russianClient.fetchAllFromUserDictionary();
+            for (CorrectionEntry correction : corrections) {
+                log(correction.singular.nominative + ":");
+                log("   Р: " + correction.singular.genitive);
+                log("   Д: " + correction.singular.dative);
+                log("   В: " + correction.singular.accusative);
+                log("   Т: " + correction.singular.instrumental);
+                log("   П: " + correction.singular.prepositional);
+                log("   М: " + correction.singular.locative);
+                if (correction.plural != null) {
+                    log("   М_Р: " + correction.plural.genitive);
+                    log("   М_Д: " + correction.plural.dative);
+                    log("   М_В: " + correction.plural.accusative);
+                    log("   М_Т: " + correction.plural.instrumental);
+                    log("   М_П: " + correction.plural.prepositional);
+                    log("   М_М: " + correction.plural.locative);
+                }
             }
+            log("");
+
+            log("Удаляем исправление.");
+            // True если исправление было удалено успешно, false если исправление не найдено.
+            boolean found = russianClient.removeFromUserDictionary("Кошка");
+            log("Исправление найдено: %s", found ? "Да" : "Нет");
+
+            log("Склонение после удаления исправления:");
+            DeclensionResult spellWithoutCorrection = russianClient.declension("Кошка");
+            log("            Именительный падеж: %s", spellWithoutCorrection.nominative);
+            log("               Дательный падеж: %s", spellWithoutCorrection.dative);
+            log("Дательный падеж множественного: %s", spellWithoutCorrection.plural.dative);
+            log("");
         }
-        log("");
-
-        log("Удаляем исправление.");
-        // True если исправление было удалено успешно, false если исправление не найдено.
-        boolean found = russianClient.removeFromUserDictionary("Кошка");
-        log("Исправление найдено: %s", found ? "Да" : "Нет");
-
-        log("Склонение после удаления исправления:");
-        DeclensionResult spellWithoutCorrection = russianClient.declension("Кошка");
-        log("            Именительный падеж: %s", spellWithoutCorrection.nominative);
-        log("               Дательный падеж: %s", spellWithoutCorrection.dative);
-        log("Дательный падеж множественного: %s", spellWithoutCorrection.plural.dative);
-        log("");
-
+        catch(NumeralsDeclensionNotSupportedException e){
+            log(e.getMessage());
+        }
+        catch(ArgumentNotRussianException e) {
+            log(e.getMessage());
+        }
+        catch(InvalidFlagsException e) {
+            log(e.getMessage());
+        }
+        catch(ArgumentEmptyException e) {
+            log(e.getMessage());
+        }
     }
 
     private static void russianAdjectivizeExample(RussianClient russianClient) throws IOException, AccessDeniedException {
@@ -135,14 +147,22 @@ public class UsageExample {
         log("");
     }
 
-    private static void russianNumberSpellingResult(RussianClient russianClient) throws IOException, NumeralsDeclensionNotSupportedException, ArgumentNotRussianException, ArgumentEmptyException, AccessDeniedException {
+    private static void russianNumberSpellingResult(RussianClient russianClient) throws IOException, AccessDeniedException {
         log("Сумма прописью:");
 
-        int number = 2513;
-        NumberSpellingResult result = russianClient.spell(number, "рубль");
-        log("В размере %s (%s) %s", number,
-                result.numberDeclension.genitive,
-                result.unitDeclension.genitive);
+        try {
+            int number = 2513;
+            NumberSpellingResult result = russianClient.spell(number, "рубль");
+            log("В размере %s (%s) %s", number,
+                    result.numberDeclension.genitive,
+                    result.unitDeclension.genitive);
+        }
+        catch(ArgumentNotRussianException e) {
+            throw new RuntimeException("Морфер не считает слово рубль русским.", e);
+        }
+        catch(ArgumentEmptyException e) {
+            throw new RuntimeException("Морфер считает слово рубль пустой строкой.", e);
+        }
     }
 
     private static void russianAdjectivesGendersExample(RussianClient russianClient) throws IOException, AccessDeniedException {
@@ -155,45 +175,79 @@ public class UsageExample {
         log("");
     }
 
-    private static void russianDeclensionsAndGenderExample(RussianClient russianClient) throws IOException, NumeralsDeclensionNotSupportedException, ArgumentNotRussianException, InvalidFlagsException, ArgumentEmptyException, AccessDeniedException {
+    private static void russianDeclensionsAndGenderExample(RussianClient russianClient) throws IOException, AccessDeniedException {
         log("Склонение на русском языке:");
 
-        DeclensionResult result = russianClient.declension("Соединенное королевство");
-        log("Именительный падеж: %s", result.nominative);
-        log(" Родительный падеж: %s", result.genitive);
-        log("   Дательный падеж: %s", result.dative);
-        log(" Винительный падеж: %s", result.accusative);
-        log("Творительный падеж: %s", result.instrumental);
-        log("  Предложный падеж: %s", result.prepositional);
-        log("     Местный падеж: %s", result.prepositionalWithO != null ? result.prepositionalWithO : PREMIUM);
-        log("               Где? %s", result.where != null ? result.where : PREMIUM);
-        log("              Куда? %s", result.to != null ? result.to : PREMIUM);
-        log("            Откуда? %s", result.from != null ? result.from : PREMIUM);
-
-        if (result.plural != null) {
-            log("Именительный падеж: %s", result.plural.nominative);
-            log(" Родительный падеж: %s", result.plural.genitive);
-            log("   Дательный падеж: %s", result.plural.dative);
-            log(" Винительный падеж: %s", result.plural.accusative);
-            log("Творительный падеж: %s", result.plural.instrumental);
-            log("  Предложный падеж: %s", result.plural.prepositional);
+        try {
+            DeclensionResult result = russianClient.declension("Соединенное королевство");
+            log("Именительный падеж: %s", result.nominative);
+            log(" Родительный падеж: %s", result.genitive);
+            log("   Дательный падеж: %s", result.dative);
+            log(" Винительный падеж: %s", result.accusative);
+            log("Творительный падеж: %s", result.instrumental);
+            log("  Предложный падеж: %s", result.prepositional);
             log("     Местный падеж: %s", result.prepositionalWithO != null ? result.prepositionalWithO : PREMIUM);
-        }
-        log("");
+            log("               Где? %s", result.where != null ? result.where : PREMIUM);
+            log("              Куда? %s", result.to != null ? result.to : PREMIUM);
+            log("            Откуда? %s", result.from != null ? result.from : PREMIUM);
 
-        log("Определение рода на русском языке:");
-        log("Род: %s", result.gender != null ? result.gender : PREMIUM);
-        log("");
+            if (result.plural != null) {
+                log("Именительный падеж: %s", result.plural.nominative);
+                log(" Родительный падеж: %s", result.plural.genitive);
+                log("   Дательный падеж: %s", result.plural.dative);
+                log(" Винительный падеж: %s", result.plural.accusative);
+                log("Творительный падеж: %s", result.plural.instrumental);
+                log("  Предложный падеж: %s", result.plural.prepositional);
+                log("     Местный падеж: %s", result.prepositionalWithO != null ? result.prepositionalWithO : PREMIUM);
+            }
+            log("");
+
+            log("Определение рода на русском языке:");
+            log("Род: %s", result.gender != null ? result.gender : PREMIUM);
+            log("");
+        }
+        catch(NumeralsDeclensionNotSupportedException e){
+            // Во входном словосочетании было числительное, например "три кота".
+            // Пока Морфер не умеет их склонять.
+            log(e.getMessage());
+        }
+        catch(ArgumentNotRussianException e) {
+            // Во входном словосочетании не нашлось русских слов, например "Vasily Pupkin".
+            // Решить проблему можно, например, добавлением родового слова.
+            // Например, если вы склоняете имена пользователей, то добавьте слово пользователь:
+            // и просклоняйте словосочетание "пользователь Vasily Pupkin".
+            log(e.getMessage());
+        }
+        catch(InvalidFlagsException e) {
+            log(e.getMessage());
+        }
+        catch(ArgumentEmptyException e) {
+            log(e.getMessage());
+        }
     }
 
-    private static void russianNamesDeclensionsExample(RussianClient russianClient) throws IOException, NumeralsDeclensionNotSupportedException, ArgumentNotRussianException, InvalidFlagsException, ArgumentEmptyException, AccessDeniedException {
-        log("Разделение ФИО на части:");
+    private static void russianNamesDeclensionsExample(RussianClient russianClient) throws IOException, AccessDeniedException {
+        try {
+            log("Разделение ФИО на части:");
 
-        DeclensionResult nameResult = russianClient.declension("Полад Бюльбюль-оглы Мамедов");
-        log("Ф: " + nameResult.fullName.surname);
-        log("И: " + nameResult.fullName.name);
-        log("О: " + nameResult.fullName.patronymic);
-        log("");
+            DeclensionResult nameResult = russianClient.declension("Полад Бюльбюль-оглы Мамедов");
+            log("Ф: " + nameResult.fullName.surname);
+            log("И: " + nameResult.fullName.name);
+            log("О: " + nameResult.fullName.patronymic);
+            log("");
+        }
+        catch(NumeralsDeclensionNotSupportedException e){
+            log(e.getMessage());
+        }
+        catch(ArgumentNotRussianException e) {
+            log(e.getMessage());
+        }
+        catch(InvalidFlagsException e) {
+            log(e.getMessage());
+        }
+        catch(ArgumentEmptyException e) {
+            log(e.getMessage());
+        }
     }
 
     private static void ukrainianSpellWithCorrectionExample(UkrainianClient ukrainianClient) throws IOException, AccessDeniedException {
