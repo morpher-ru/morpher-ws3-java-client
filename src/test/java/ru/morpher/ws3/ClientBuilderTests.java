d58963e1b3abc3a27615848d67716930671fc564
Sergey Slepov <slepov@gmail.com>
2017-12-09 00:20:54 +0000

    Add failing test CallingBuildTwiceWorks.

---
+++ src/test/java/ru/morpher/ws3/ClientBuilderTests.java
@@ -0,0 +1,23 @@
+package ru.morpher.ws3;
+
+import org.junit.Test;
+import ru.morpher.ws3.communicator.CommunicatorStub;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+public class ClientBuilderTests {
+
+    @Test
+    public void CallingBuildTwiceWorks() throws IOException, AccessDeniedException {
+        String baseUrl = "https://ws3.morpher.ru";
+        CommunicatorStub communicator = new CommunicatorStub();
+        ClientBuilder builder = new ClientBuilder().use(communicator).useToken("token");
+        builder.build();
+        Client client = builder.build();
+        communicator.writeNextResponse("1");
+        client.queriesLeftForToday();
+        assertEquals(baseUrl + "/get_queries_left_for_today", communicator.readLastUrlPassed());
+    }
+}
